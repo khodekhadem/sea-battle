42fc045f76d8fc53557e251ba682cf233dd45325
khodekhadem <khadem13khadem@gmail.com>
2022-12-08 16:57:00 +0330

    complete faze 1

---
+++ faze1.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+int faz1(int *p,int n){
+    printf("@");
+    for(int i=0;i<n;i++){printf("%d",i);}
+    printf("\n");
+    for(int i=0;i<n;i++){
+        printf("%d",i);
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d",*p);
+            *p = *(p+1);
+        }
+        printf("\n");
+    }
+
+
+}
+void printer(int *p,int n){
+
+    for(int i=0;i<n;i++){
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d",*p++);
+        }
+        printf("\n");
+    }
+
+
+
+
+}
+
+//0 water
+//1 boat
+//2 damaged boat
+//3 singed dot 
+int main(){
+    int n,tmpx,tmpy;
+    scanf("%d",&n);
+    int board[n][n];
+    
+    for(int i=0;i<n;i++){
+        for (int j = 0; j < n; j++)
+        {
+            board[i][j]=0;
+        }
+    }
+
+
+
+
+
+    for(int i=0;i<3;i++){
+        scanf("%d%d",&tmpx,&tmpy);
+        board[tmpx][tmpy]=2;
+    }
+    printer(&board[0][0],n);
+    //faz1(&board[0][0],n);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;}
