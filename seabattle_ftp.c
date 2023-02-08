7060aca59254a5cd4c3c2d876fac7a49f12c39b5
khodekhadem <khadem13khadem@gmail.com>
2023-02-08 11:52:46 +0330

    pftp problem solved

--- seabattle_ftp.c
+++ seabattle_ftp.c
@@ -4,7 +4,8 @@ void call_upload(){
 #if WIN32
     char *cmd = "ftp -s:.\\ftpc\\WINftp-upload.txt";
 #elif __linux__
-    char *cmd = "pftp -n < ftpc/ftp-upload";
+    char *cmd = "pftp -n < ftpc/ftp-upload"; //comment if you dont have pftp
+    //char *cmd = "ftp -p -n < ftpc/ftp-upload"; // uncomment if you dont have pftp
 #endif
 
     system(cmd);
@@ -14,7 +15,10 @@ void call_download(){
 #if WIN32
     char *cmd = "ftp -s:.\\ftpc\\WINftp-download.txt";
 #elif __linux__
-    char *cmd = "pftp -n < ftpc/ftp-download";
+    char *cmd = "pftp -n < ftpc/ftp-download";// uncomment if you dont have pftp
+
+    //char *cmd = "ftp -p -n < ftpc/ftp-download";// uncomment if you dont have pftp
+
 #endif
 
     system(cmd);
